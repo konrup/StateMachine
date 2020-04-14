c87f25321fd099cadce05615f663b1c320b86640
konrup <20823572+konrup@users.noreply.github.com>
2020-04-14 11:20:05 +0200

    Add test to check jenkins setup

--- src/test/java/StateMachine/LibraryTest.java
+++ src/test/java/StateMachine/LibraryTest.java
@@ -7,8 +7,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+
+  @Test
+  public void testSomeLibraryMethod() {
+    Library classUnderTest = new Library();
+    assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+  }
+
+  @Test
+  public void testFail() {
+    throw new RuntimeException("This should fail the Jenkins build");
+  }
 }
